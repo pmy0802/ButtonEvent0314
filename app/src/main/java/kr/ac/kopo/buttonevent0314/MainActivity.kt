793949c87b6c4d44cce3949fe468794519f6b80f
pmy0802 <weerty3732@gmail.com>
2024-03-14 16:04:02 +0900

    4개의 각 버튼 이벤트 처리

--- app/src/main/java/kr/ac/kopo/buttonevent0314/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/buttonevent0314/MainActivity.kt
@@ -1,5 +1,7 @@
 package kr.ac.kopo.buttonevent0314
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,11 +9,39 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var btn : Button
+    lateinit var btnHome : Button
+    lateinit var btn911 : Button
+    lateinit var btnGallery : Button
+    lateinit var btnFinish : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn = findViewById(R.id.button)
+        btnHome = findViewById(R.id.buttonHome)
+        btn911 = findViewById(R.id.button911)
+        btnGallery = findViewById(R.id.buttonGallery)
+        btnFinish = findViewById(R.id.buttonfinish)
+
+        btnHome.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kopo.ac.kr/jungsu"))
+            startActivity(intent)
+        }
+
+        btn911.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"))
+            startActivity(intent)
+        }
+
+        btnGallery.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/intemal/images/media"))
+            startActivity(intent)
+        }
+
+        btnFinish.setOnClickListener {
+            finish()
+        }
+
         btn.setOnClickListener {
             Toast.makeText(applicationContext, "기분 좋은 오후입니다.", Toast.LENGTH_SHORT).show()
         }
