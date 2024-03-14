3cf8d6e204cccaaeb50ab5aa72654f4e9f592ade
pmy0802 <weerty3732@gmail.com>
2024-03-14 16:24:14 +0900

    when 사용하여 이벤트 처리

--- app/src/main/java/kr/ac/kopo/buttonevent0314/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/buttonevent0314/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btn911 : Button
     lateinit var btnGallery : Button
     lateinit var btnFinish : Button
+    lateinit var btnListener : View.OnClickListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,20 +25,19 @@ class MainActivity : AppCompatActivity() {
         btnGallery = findViewById(R.id.buttonGallery)
         btnFinish = findViewById(R.id.buttonfinish)
 
-        btnHome.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kopo.ac.kr/jungsu"))
+        btnListener = View.OnClickListener {
+            when(it.id){
+                R.id.buttonHome -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kopo.ac.kr/jungsu"))
+                R.id.button911 -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"))
+                R.id.buttonGallery -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/intemal/images/media"))
+
+            }
             startActivity(intent)
         }
 
-        btn911.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"))
-            startActivity(intent)
-        }
-
-        btnGallery.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/intemal/images/media"))
-            startActivity(intent)
-        }
+        btnHome.setOnClickListener(btnListener)
+        btn911.setOnClickListener(btnListener)
+        btnGallery.setOnClickListener(btnListener)
 
         btnFinish.setOnClickListener {
             finish()
